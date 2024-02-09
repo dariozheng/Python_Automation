72851185d338ca1e8b41817974deca7f26a7a8dd
Dario Zheng <109980564+dariozheng@users.noreply.github.com>
2024-02-09 11:35:40 +0100

    Update and rename Regex_phoneAndEmail.py to Regex_PhoneAndEmail.py

--- Regex_PhoneAndEmail.py
+++ Regex_PhoneAndEmail.py
@@ -1,5 +1,5 @@
 #! python3
-# phoneAndEmail.py - Finds phone numbers and email addresses on the clipboard.
+# PhoneAndEmail.py - Finds phone numbers and email addresses on the clipboard.
 import pyperclip, re
 
 phoneRegex = re.compile(r'''(
